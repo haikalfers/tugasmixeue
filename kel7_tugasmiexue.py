4d2dd4dc8ad0e49327e35ac287f3f2ad0ced2572
alyafputri <145920130+alyafputri@users.noreply.github.com>
2024-03-13 14:14:29 +0700

    Update kel7_tugasmiexue.py

--- kel7_tugasmiexue.py
+++ kel7_tugasmiexue.py
@@ -8,3 +8,13 @@ class Node:
 class Orders:
     def __init__(self, data=None):
         self.head = Node(data) if data else None
+
+    def tambah_pesanan(self, menu, harga):
+        new_node = Node(menu, harga)
+        if not self.head:
+            self.head = new_node
+        else:
+            current = self.head
+            while current.next:
+                current = current.next
+            current.next = new_node
